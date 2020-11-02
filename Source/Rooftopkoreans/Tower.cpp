7ee40361776574d87e9ddec165b2a393474f132a
RaronRarenberg <43893501+RaronRarenberg@users.noreply.github.com>
2020-11-02 21:21:34 +0100

    KURWA CPP

--- Source/Rooftopkoreans/Tower.cpp
+++ Source/Rooftopkoreans/Tower.cpp
@@ -22,88 +22,13 @@ void ATower::BeginPlay()
 void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (TowerTarget != nullptr)
-	{
-		SetTargetRotation();
-		if (!IsReloading)
-		{
-			//Shot();
-		}
+	
 	}
 	else
 	{
 		//CheckForNewTarget();
 	}
 
-}
-
-void ATower::SetTargetRotation()
-{
-	
-	FRotator NewRotation = UKismetMathLibrary::FindLookAtRotation(this->GetActorLocation(), TowerTarget->GetActorLocation());
-	SetActorRotation(NewRotation);
-}
-
-void ATower::SetName(FString ComingName)
-{
-	Name = ComingName;
-}
-
-FString ATower::GetName()
-{
-	return Name;
-}
-
-void ATower::SetDamage(float ComingDamage)
-{
-	if(ComingDamage > 0)
-	Damage = ComingDamage;
-}
-
-float ATower::GetDamage()
-{
-	return Damage;
-}
-
-void ATower::SetRange(float ComingRange)
-{
-	Range = ComingRange;
-}
-
-float ATower::GetRange()
-{
-	return Range;
-}
-
-void ATower::SetFireRate(float ComingFireRate)
-{
-	FireRate = ComingFireRate;
-}
-
-float ATower::GetFireRate()
-{
-	return FireRate;
-}
-
-void ATower::SetCost(int32 ComingCost)
-{
-	if (ComingCost > 0)
-	Cost = ComingCost;
-}
-
-int32 ATower::GetCost()
-{
-	return Cost;
-}
-
-void ATower::CheckForNewTarget()
-{
-	CheckForTargetRate = true;
-	const FVector Start = this->GetActorLocation();
-	const FVector End = Start - FVector(0.0f,0.0f,1.0f);
-	FCollisionShape MySphere = FCollisionShape::MakeSphere(GetRange()); 
-	TArray<FHitResult> OutResults;
-	SphereTraceMu
 }
 
 
--- Source/Rooftopkoreans/Tower.h
+++ Source/Rooftopkoreans/Tower.h
@@ -27,30 +27,10 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	void CheckForNewTarget();
-	void SetTargetRotation();
-	void Shot();
-	void SetName(FString ComingName);
-	FString GetName();
-	void SetDamage(float ComingDamage);
-	float GetDamage();
-	void SetRange(float ComingRange);
-	float GetRange();
-	void SetFireRate(float ComingFireRate);
-	float GetFireRate();
-	void SetCost(int32 ComingCost);
-	int32 GetCost();
+	
 
 private:
-	AEnemy* TowerTarget;
-	bool IsReloading = false;
-	bool CheckForTargetRate = false;
-
-	FString Name;
-	float Damage;
-	float Range;
-	float FireRate;
-	int32 Cost;
+	
 
 
 };
